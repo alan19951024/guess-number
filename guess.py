3f4934e1a484e7bd0da627d0d767ef6658a1cb3c
alan YU <alan19951024@gmail.com>
2020-01-10 16:40:14 +0800

    final version

--- guess.py
+++ guess.py
@@ -3,15 +3,19 @@
 #猜對的話印出"終於猜對了"
 #猜錯的話 要告訴他 比答案大或小
 import random
-r = random.randint(1,100)
+start = input('請決定隨機數字範圍開始值:')
+end = input('請決定隨機數字範圍結束值:')
+start = int(start)
+end = int(end)
+r = random.randint(start,end)
 count = 0
 while True:
 	count +=1  #count = count + 1
-	one = input('1~100請猜一個數字:')
+	one = input('請猜一個數字:')
 	one = int(one)
 	if one == r:
 		print('恭喜你,猜對了')
-		print('你猜的第', count, '次')
+		print('這是你猜的第', count, '次')
 		break
 	elif one > r:
 		print('比答案大')
